26d4f4d9700070d05eedab743b81101e4a86d568
kalpeshshirsath11 <kalpeshshirsath11@gmail.com>
2026-01-26 18:45:12 +0530

    enviroment variable setup

--- Backend/src/main/java/com/Ajinkya/Infotech/InfotechApplication.java
+++ Backend/src/main/java/com/Ajinkya/Infotech/InfotechApplication.java
@@ -1,5 +1,6 @@
 package com.Ajinkya.Infotech;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -9,6 +10,14 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class InfotechApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure()
+				.ignoreIfMissing()
+				.load();
+
+		dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue())
+		);
 		SpringApplication.run(InfotechApplication.class, args);
 	}
 
